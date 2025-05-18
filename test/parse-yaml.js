ba2bc22b06834a2878d55b75312d20e9f39448e8
erikzaadi <erik.zaadi@gmail.com>
2025-05-18 08:07:28 +0300

    Remove deprecated safe prefix for js-yaml

--- test/parse-yaml.js
+++ test/parse-yaml.js
@@ -53,9 +53,9 @@ describe('parse YAML:', function() {
     assert(actual.hasOwnProperty('orig'));
   });
 
-  it('should use safeLoad when specified', function() {
+  it('should use safe by default', function() {
     var fixture = '---\nabc: xyz\nversion: 2\n---\n\n<span class="alert alert-info">This is an alert</span>\n';
-    var actual = matter(fixture, {safeLoad: true});
+    var actual = matter(fixture);
     assert.deepEqual(actual.data, {abc: 'xyz', version: 2});
     assert.equal(actual.content, '\n<span class="alert alert-info">This is an alert</span>\n');
     assert(actual.hasOwnProperty('orig'));
